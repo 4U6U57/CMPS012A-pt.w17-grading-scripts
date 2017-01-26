df548355a6b9fce9b65eec276bda531ddd51f35c
August Valera <4U6U57@gmail.com>
2017-01-26 15:10:14 -0800

    Add forall script, editorconfig

---
+++ lib/forall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# forall.sh
+# Runs input $CMD forall submissions in $ASG directory
+# Usage: forall $ASG $CMD
+# Or, if running from within directory named $ASG (such as $CLASS/$ASG or $CLASS/bin/$ASG)
+#        forall $CMD
+# August Valera <avalera>
+
+PwdDir=$(pwd)
+Pwd=$(basename $PwdDir)
+ClassDir=$(echo $PwdDir | cut -d "/" -f -5)
+Class=$(basename $ClassDir)
+Asg=""
+
+if [[ -e "$ClassDir/$Pwd" ]]; then
+  Asg=$Pwd
+  echo "Asg $Asg taken from basename of current directory"
+elif [[ -e "$ClassDir/$1" ]]; then
+  Asg=$1
+  echo "Asg $Asg taken from first argument of script"
+  shift
+else
+  echo "No asg provided"
+  exit
+fi
+AsgDir=$ClassDir/$Asg
+if [[ ! -d "$AsgDir" ]]; then
+  echo "ERROR: Invalid asg $Asg"
+  exit 1
+fi
+
+cd $AsgDir
+for Student in $(ls -d */); do
+  StudentDir=$AsgDir/$Student
+  cd $StudentDir
+  $@
+done
