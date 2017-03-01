c73efc92cc66b396edb23c7a256d15eff9338191
legendddhgf <legendddhgf@gmail.com>
2017-02-28 23:10:14 -0800

    Timeout added, basic functionality test works

--- pa5/pa5.sh
+++ pa5/pa5.sh
@@ -6,7 +6,7 @@
 SRCDIR=https://raw.githubusercontent.com/legendddhgf/CMPS012A-pt.w17-grading-scripts/master/pa5
 # Get all necessary extras
 
-#curl $SRCDIR/QueensUnitTests.java > QueensUnitTests.java
+curl $SRCDIR/QueensUnitTests.java > QueensUnitTests.java
 
 if [ ! -d .backup ]; then
    mkdir .backup
@@ -31,70 +31,70 @@ TEST10="-v 11"
 echo "If nothing between '=' signs, then test is passed::"
 echo "Test 1:"
 echo "=========="
-Queens $TEST1 > out1.txt
+timeout 0.5 Queens $TEST1 > out1.txt
 diff -bBwu out1.txt model-out1.txt > diff1.txt
 cat diff1.txt
 echo "=========="
 
 echo "Test 2:"
 echo "=========="
-Queens $TEST2 > out2.txt
+timeout 0.5 Queens $TEST2 > out2.txt
 diff -bBwu out2.txt model-out2.txt > diff2.txt
 cat diff2.txt
 echo "=========="
 
 echo "Test 3:"
 echo "=========="
-Queens $TEST3 > out3.txt
+timeout 0.5 Queens $TEST3 > out3.txt
 diff -bBwu out3.txt model-out3.txt > diff3.txt
 cat diff3.txt
 echo "=========="
 
 echo "Test 4:"
 echo "=========="
-Queens $TEST4 > out4.txt
+timeout 0.5 Queens $TEST4 > out4.txt
 diff -bBwu out4.txt model-out4.txt > diff4.txt
 cat diff4.txt
 echo "=========="
 
 echo "Test 5:"
 echo "=========="
-Queens $TEST5 > out5.txt
+timeout 0.5 Queens $TEST5 > out5.txt
 diff -bBwu out5.txt model-out5.txt > diff5.txt
 cat diff5.txt
 echo "=========="
 
 echo "Test 6:"
 echo "=========="
-Queens $TEST6 > out6.txt
+timeout 0.5 Queens $TEST6 > out6.txt
 diff -bBwu out6.txt model-out6.txt > diff6.txt
 cat diff6.txt
 echo "=========="
 
 echo "Test 7:"
 echo "=========="
-Queens $TEST7 > out7.txt
+timeout 0.5 Queens $TEST7 > out7.txt
 diff -bBwu out7.txt model-out7.txt > diff7.txt
 cat diff7.txt
 echo "=========="
 
 echo "Test 8:"
 echo "=========="
-Queens $TEST8 > out8.txt
+timeout 0.5 Queens $TEST8 > out8.txt
 diff -bBwu out8.txt model-out8.txt > diff8.txt
 cat diff8.txt
 echo "=========="
 
 echo "Test 9:"
 echo "=========="
-Queens $TEST9 > out9.txt
+timeout 2 Queens $TEST9 > out9.txt
 diff -bBwu out9.txt model-out9.txt > diff9.txt
 cat diff9.txt
 echo "=========="
 
 echo "Test 10:"
 echo "=========="
-Queens $TEST10 > out10.txt
+timeout 2 Queens $TEST10 > out10.txt
 diff -bBwu out10.txt model-out10.txt > diff10.txt
 cat diff10.txt
 echo "=========="
