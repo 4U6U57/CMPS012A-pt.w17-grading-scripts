7cc9759136663da18c16e610b8504d1e4d959d80
legendddhgf <legendddhgf@gmail.com>
2017-03-04 16:22:59 -0800

    Sometimes, students have infinite loops

--- pa3/pa3.sh
+++ pa3/pa3.sh
@@ -20,7 +20,7 @@ echo "If nothing between '=' signs, then test is passed::"
 for NUM in $(seq 1 6); do
   echo "Test $NUM:"
   echo "=========="
-  java GCD < in$NUM.txt > out$NUM.txt
+  timeout 1 java GCD < in$NUM.txt > out$NUM.txt
   diff -bBwu out$NUM.txt model-out$NUM.txt > diff$NUM.txt
   cat diff$NUM.txt
   echo "=========="
